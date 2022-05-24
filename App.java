355c6a9d84bb83e44d75df7b2fdc57508677c379
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-05-24 00:01:47 -0300

    exercicio 4

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 
 /*
-3) Crie um método mostra(), que não recebe nem devolve parâmetro algum e simplesmente imprime todos os atributos do nosso funcionário. Dessa maneira, você não precisa ficar copiando e colando um monte de System.out.println() para cada mudança e teste que fizer com cada um de seus funcionários, você simplesmente vai fazer:
+3) Construa dois funcionários com o new e compare-os com o ==. E se eles tiverem os mesmos atributos? Para isso você vai precisar criar outra referência:
 */
 public class App {
 
@@ -9,15 +9,18 @@ public class App {
 
         // declare 
         Funcionario f1 = new Funcionario("Allan Gaiteiro", "CTO", 25000, "311.23423.323-23");
-        Funcionario f2 = new Funcionario();
+        Funcionario f2 = new Funcionario("Allan Gaiteiro", "CTO", 25000, "311.23423.323-23");
+        //Funcionario f3 = new Funcionario();
 
         // change value
-        f2.name = "Hugo";
-        f2.salary = 1000;
-        f2.increaseSalary(50);
+        //f2.name = "Hugo";
+        //f2.salary = 1000;
+        //f2.increaseSalary(50);
 
         // show
         f1.show();
         f2.show();
+        System.out.println("Funcionario f1 "+(f1 == f2?"":"não")+ " é igual a funcionario f2.");
+        // eles não são iguais pois esta comparando a referencia e não os valores
     }
 }
