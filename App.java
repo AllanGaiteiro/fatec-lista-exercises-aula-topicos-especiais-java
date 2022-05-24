9d5f4ee74fdeea5351e8df52470aa6405602bf81
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-05-23 23:47:06 -0300

    exercice 2

--- App.java
+++ App.java
@@ -1,9 +1,13 @@
-/**
- * App
- */
+
+/*
+2) Transforme o modelo acima em uma classe Java. Teste-a, usando uma outra classe que tenha o main. Você deve criar a classe do funcionário com o nome Funcionario, mas pode nomear como quiser a classe de testes, contudo, ela deve possuir o método main.
+*/
 public class App {
 
     public static void main(String[] args) {
         System.out.println("App - Started");
+        Funcionario f1 = new Funcionario("Allan Gaiteiro", "CTO", 25000, "311.23423.323-23");
+
+        System.out.println(f1.name);
     }
 }
--- Funcionario.java
+++ Funcionario.java
@@ -1,11 +1,5 @@
+// Exercicie 2
 import java.util.Date;
-/*
-1) Modele um funcionário. Ele deve ter o nome do funcionário, o departamento onde trabalha, seu salário (double), a data de entrada no banco (String) e seu RG (String).
-
-Você deve criar alguns métodos de acordo com sua necessidade. Além deles, crie um método recebeAumento que aumenta o salario do funcionário de acordo com o parâmetro passado como ar- gumento. Crie também um método calculaGanhoAnual, que não recebe parâmetro algum, devolvendo o valor do salário multiplicado por 12..
-
-A ideia aqui é apenas modelar, isto é, só identifique que informações são importantes e o que um funci- onário faz. Desenhe no papel tudo o que um Funcionario tem e tudo que ele faz.
-*/
 public class Funcionario {
     public String name;
     public String department;
